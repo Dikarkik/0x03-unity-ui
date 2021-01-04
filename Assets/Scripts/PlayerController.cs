52815918518c5c743869aa37c06acb53384dfe19
KaroDev <karodev3@gmail.com>
2021-01-04 14:54:09 -0500

    All tasks (UI and Menu Scene)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private int score = 0;
     public int health = 5;
+    public Text scoreText;
+    public Text healthText;
+    public Text winLoseText;
+    public Image winLoseBG;
 
     // movement and rotation
     public float speed;
@@ -14,7 +20,33 @@ public class PlayerController : MonoBehaviour
 
     void Start() => rb = GetComponent<Rigidbody>();
 
-	void FixedUpdate()
+	void SetScoreText() => scoreText.text = $"Score: {score}";
+
+	void SetHealthText() => healthText.text = $"Health: {health}";
+
+    void DisplayYouWin()
+    {
+        winLoseText.text = $"You Win!";
+        winLoseText.color = Color.black;
+        winLoseBG.color = Color.green;
+        winLoseBG.transform.gameObject.SetActive(true);
+    }
+
+    void DisplayYouLose()
+    {
+        winLoseText.text = $"Game Over!";
+        winLoseText.color = Color.white;
+        winLoseBG.color = Color.red;
+        winLoseBG.transform.gameObject.SetActive(true);
+    }
+
+    IEnumerator LoadScene(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    void FixedUpdate()
     {
             // movement
             forceDirection.x = Input.GetAxis("Horizontal");
@@ -31,7 +63,13 @@ public class PlayerController : MonoBehaviour
 	{
         if(health == 0)
         {
-            Debug.Log("Game Over!");
+            //Debug.Log("Game Over!");
+            StartCoroutine(LoadScene(3));
+            DisplayYouLose();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
             SceneManager.LoadScene(0, LoadSceneMode.Single);
         }
     }
@@ -43,15 +81,19 @@ public class PlayerController : MonoBehaviour
             case "Pickup":
                 score++;
                 Destroy(other.gameObject);
-                Debug.Log($"Score: {score}");
+                //Debug.Log($"Score: {score}");
+                SetScoreText();
                 break;
             case "Trap":
                 health--;
-                Debug.Log($"Health: {health}");
+                SetHealthText();
+                //Debug.Log($"Health: {health}");
                 break;
             case "Goal":
-                Debug.Log("You win!");
-                break;
+                //Debug.Log("You win!");
+                StartCoroutine(LoadScene(3));
+                DisplayYouWin();
+            break;
         }
     }
 }
